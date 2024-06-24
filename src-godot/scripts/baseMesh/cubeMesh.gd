75576faab33b07ccb2f65792f74fe4581e96e6a4
fflores <francisco.floresfanelli@gmail.com>
2024-06-24 01:53:14 -0300

    cubeMesh

--- src-godot/scripts/baseMesh/cubeMesh.gd
+++ src-godot/scripts/baseMesh/cubeMesh.gd
@@ -5,3 +5,19 @@ extends Node3D
 @onready var mesh = $StaticBody3D/MeshInstance3D
 
 
+var x = 0
+var y = 0
+var z = 0
+
+func _ready():
+	pass
+
+
+
+func _on_button_pressed():
+	mesh.mesh.size.x += 1
+
+
+func _on_button_2_pressed():
+	if mesh.mesh.size.x > 0:
+		mesh.mesh.size.x -= 1
