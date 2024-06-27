c0f6213d4be7c36af0381c9c5ae0f418d3f895b9
fflores <francisco.floresfanelli@gmail.com>
2024-06-27 00:40:56 -0300

    raycast

--- src-godot/scripts/cameraBase.gd
+++ src-godot/scripts/cameraBase.gd
@@ -1,5 +1,9 @@
 class_name FreeLookCamera extends Camera3D
 
+@onready var raycast = $RayCast3D
+@export var ray_length : float = 1000
+var last_hovered: Node3D = null
+
 # Modifier keys' speed multiplier
 const SHIFT_MULTIPLIER = 2.5
 const ALT_MULTIPLIER = 1.0 / SHIFT_MULTIPLIER
@@ -27,6 +31,9 @@ var _q = false
 var _e = false
 var _shift = false
 var _alt = false
+
+func _ready():
+	$RayCast3D.enabled = true
 
 func _input(event):
 	# Receives mouse motion
@@ -67,6 +74,42 @@ func _input(event):
 func _process(delta):
 	_update_mouselook()
 	_update_movement(delta)
+	
+	var mouse_pos: Vector2 = get_viewport().get_mouse_position()
+	var from: Vector3 = project_ray_origin(mouse_pos)
+	var to: Vector3 = from + project_ray_normal(mouse_pos) * ray_length
+	
+	 # Configurar el RayCast3D
+	raycast.target_position = to - from
+	raycast.force_raycast_update()
+	
+	checkColideRaycas()
+	
+func checkColideRaycas() -> void:
+	
+	if raycast.is_colliding():
+		print('check is_colliding')
+		var collider: Node3D = raycast.get_collider()
+		print('colider', collider)
+		if collider != last_hovered:
+			if last_hovered != null:
+				reset_hover_color(last_hovered)
+			apply_hover_color(collider)
+			last_hovered = collider
+	else:
+		if last_hovered != null:
+			reset_hover_color(last_hovered)
+			last_hovered = null
+
+func apply_hover_color(collider: Node3D) -> void:
+	if collider is MeshInstance3D:
+		var material = StandardMaterial3D.new()
+		material.albedo_color = Color(1, 1, 0)  # Cambia al color amarillo
+		collider.material_override = material
+
+func reset_hover_color(collider: Node3D) -> void:
+	if collider is MeshInstance3D:
+		collider.material_override = null  # Restablece al material original
 
 # Updates camera movement
 func _update_movement(delta):
